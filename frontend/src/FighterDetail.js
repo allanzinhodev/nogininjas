1d82d53dcae6109000fe6a7c19c102e121fc111e
Allanzinho <allanrd94@gmail.com>
2024-11-04 22:05:50 -0300

    Correção do Bootstrap

--- frontend/src/FighterDetail.js
+++ frontend/src/FighterDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Card, Spinner, Alert } from 'react-bootstrap';
+import { Container, Col, Row, Spinner, Alert, Image } from 'react-bootstrap';
 
 function FighterDetail({ fighterId }) {
     const [fighter, setFighter] = useState(null);
@@ -33,21 +33,22 @@ function FighterDetail({ fighterId }) {
     console.log(fighter);
 
     return (
-        <Container fluid className='fighterCard'>
-            <h2>Detalhes do Lutador</h2>
+        <Container className='fighterCard'>
             {fighter ? (
-                <Card className='transparent-card'>
-                    <Card.Body>
-                        <Card.Title>{fighter.name}</Card.Title>
-                        <Card.Text>
-                            <strong>Altura:</strong> {fighter.height}<br />
-                            <strong>País:</strong> {fighter.country}<br />
-                            <strong>Idade:</strong> {fighter.age}<br />
-                            <strong>Equipe:</strong> {fighter.team}<br />
-                            <strong>Sobre:</strong> {fighter.about}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <Row>
+                    <Col md = {4}>
+                        <Image src={`../figther/thumb/${fighter.id}.png`} fluid />
+                    </Col>
+                    <Col md ={8}>
+                        <h1>{fighter.name}</h1>
+                        <strong>Altura:</strong> {fighter.height}<br />
+                        <strong>País:</strong> {fighter.country}<br />
+                        <strong>Idade:</strong> {fighter.age}<br />
+                        <strong>Equipe:</strong> {fighter.team}<br />
+                        <strong>Sobre:</strong> {fighter.about}
+                    </Col>
+                </Row>
+
             ) : (
                 <Alert variant="warning">Lutador não encontrado.</Alert>
             )}
