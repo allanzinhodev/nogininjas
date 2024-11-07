dba583a13390588f7c0e8b69ddde4a90ca8faae4
Allanzinho <allanrd94@gmail.com>
2024-11-07 18:42:38 -0300

    Vercel Test

--- db.js
+++ db.js
@@ -1,13 +1,6 @@
-require('dotenv').config(); // Carregar as variáveis do .env
-
-const { Pool } = require('pg');
+const { Pool } = require('pg')
+require('dotenv').config()
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS, // A senha deve ser uma string
-    port: process.env.DB_PORT,
-});
-
-module.exports = pool;
+  connectionString: process.env.POSTGRES_URL,
+})
