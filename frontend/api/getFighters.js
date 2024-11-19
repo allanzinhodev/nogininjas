fe0f5befbb3d73bacd8a15af257430a5a10d3909
Allanzinho <allanrd94@gmail.com>
2024-11-18 23:51:00 -0300

    retest

--- frontend/api/getFighters.js
+++ frontend/api/getFighters.js
@@ -9,11 +9,9 @@ export default async function handler(req, res) {
 
   try {
     await client.connect();
-    const result = await client.query('SELECT * FROM fighter WHERE id = $1');
-    console.log('Query Result:', result.rows);  // Log para verificar os dados retornados
+    const result = await client.query('SELECT * FROM fighters');
     res.status(200).json(result.rows);
   } catch (error) {
-    console.error('Database Connection Error:', error);  // Log do erro
     res.status(500).json({ error: 'Failed to fetch data' });
   } finally {
     await client.end();
